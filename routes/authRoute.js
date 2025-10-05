cdb3b5f45b0d28f575edcef2d1b2fc54e41f30dc
vithalaparachintan <vithalaparachintan@gmail.com>
2025-10-05 17:23:27 +0530

    Fix: correct controller path

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 
-const { registerUser, loginUser } = require("../controllers/authcontroller");
+const { registerUser, loginUser } = require("../controllers/authController");
 
 router.post("/register", registerUser);
 router.post("/login", loginUser);
