8305dc409e8e91d60b6b8e6101fd13fdfe191252
vithalaparachintan <vithalaparachintan@gmail.com>
2025-10-06 17:40:26 +0530

    change in Customer.js

--- models/Customer.js
+++ models/Customer.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const customerSchema = new mongoose.Schema({
   user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
   name: { type: String, required: true },
-  phone: { type: String },
+  phone: { type: String,required:true },
   balance: { type: Number, default: 0 }
 }, { timestamps: true });
 
